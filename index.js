f2b6ebce9c8fb179e578272f5a1e66e0757f4a8f
madihaHaque9 <madia.haque@gmail.com>
2024-03-29 17:26:04 +0600

    existing user redirect

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ async function run() {
     const cartCollection=client.db("bisstroDb").collection("carts");
     app.post('/users',async(req,res)=>{
       const user=req.body;
+      const query={email:user.email}
+      const existingUser=await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message:'user already exists',insertedId:null})
+      }
+
       const result=await userCollection.insertOne(user);
       res.send(result);
     })
