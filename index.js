00e6295ea1f04e67479f72ec872bb99ea8c63bed
madihaHaque9 <madia.haque@gmail.com>
2024-03-29 23:13:59 +0600

    update user to admin

--- index.js
+++ index.js
@@ -26,6 +26,30 @@ async function run() {
     const userCollection=client.db("bisstroDb").collection("users");
     const reviewCollection=client.db("bisstroDb").collection("reviews");
     const cartCollection=client.db("bisstroDb").collection("carts");
+    app.get('/users',async(req,res)=>{
+      const result=await userCollection.find().toArray();
+      res.send(result)
+    })
+    app.patch('/users/admin/:id',async(req,res)=>{
+      const id= req.params.id;
+      const filter={_id: new ObjectId(id)};
+      const updatedDoc={
+        $set:{
+          role:"admin"
+
+        }
+      }
+      const result=await userCollection.updateOne(filter,updatedDoc);
+      res.send(result);
+
+    })
+    app.delete('/users/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+
+    })
     app.post('/users',async(req,res)=>{
       const user=req.body;
       const query={email:user.email}
